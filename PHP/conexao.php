48efcb489ddb6ec5c944187faa3b5b2f874dd610
hugostangherlin <hugostangherlin@gmail.com>
2025-02-27 15:15:33 -0300

    27/02
    *Dados adicionados ao banco de dados do site
    *PDO feito, porém ainda não conectou ao xampp para vê se a conexão foi concluída

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,2 +1,16 @@
 <?php 
+$host ="localhost";
+$user = "root";
+$pass = "";
+$dbname = "rodeo_hotel";
+$port = 3306;
+
+try{
+
+$conn = new PDO(dsn:"mysql:host=$host;port=$port;dbname=". $dbname, username: $user, password: $pass);
+echo "Conexão com o banco de dados efetuada com sucesso";
+}catch (PDOException $err){
+    echo "ERROR: Conexão com o banco de dados efetuada não realizada". $err -> getMessage();
+
+}
 ?>
--- sql/hotel.sql
+++ sql/hotel.sql
@@ -28,4 +28,31 @@ CREATE TABLE Categoria (
     Descrisão TEXT
 );
 
+CREATE TABLE Categoria (
+    ID_Categoria INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(50) NOT NULL,
+    Descricao TEXT
+);
+
+CREATE TABLE Quartos (
+    ID_Quarto INT AUTO_INCREMENT PRIMARY KEY,
+    ID_Categoria INT,
+    Status ENUM('Disponível', 'Ocupado', 'Manutenção') NOT NULL,
+    Capacidade INT NOT NULL,
+    FOREIGN KEY (ID_Categoria) REFERENCES Categoria(ID_Categoria)
+);
+
+CREATE TABLE Reservas (
+    ID_Reserva INT AUTO_INCREMENT PRIMARY KEY,
+    ID_Cliente INT,
+    ID_Quarto INT,
+    ID_Funcionario INT,
+    Status ENUM('Confirmada', 'Pendente', 'Cancelada', 'Finalizada') NOT NULL,
+    Check_in DATE NOT NULL,
+    Check_out DATE NOT NULL,
+    Data_Reserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (ID_Cliente) REFERENCES Clientes(ID_Cliente),
+    FOREIGN KEY (ID_Quarto) REFERENCES Quartos(ID_Quarto),
+    FOREIGN KEY (ID_Funcionario) REFERENCES Funcionarios(ID_Funcionario)
+);
 
