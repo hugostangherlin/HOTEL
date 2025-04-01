7277b6e7145e0c51072b206484964d053dc3bfe8
Hugo <hugostangherlin@gmail.com>
2025-03-31 21:30:00 -0300

    Update adicionarquarto.php

--- admin/PHP/adicionarquarto.php
+++ admin/PHP/adicionarquarto.php
@@ -1,12 +1,11 @@
 <?php
 require 'conexao.php';
 
-$category = filter_input(INPUT_POST, 'category');
-$status = filter_input(INPUT_POST, 'status');
-$capacity = filter_input(INPUT_POST, 'capacity', FILTER_VALIDATE_INT);
-$price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
 
-if ($category && $status && $capacity && $price) {
+$status = filter_input(INPUT_POST, 'status');
+$capacity = filter_input(INPUT_POST, 'capacity');
+
+if ( $status && $capacity) {
 
     $sql = $pdo->prepare("SELECT * FROM quartos WHERE id_quarto = :id");
     $sql->bindParam(':id', $id, PDO::PARAM_INT);
@@ -15,10 +14,8 @@ if ($category && $status && $capacity && $price) {
     if ($sql->rowCount() === 0) {
         $sql = $pdo->prepare("INSERT INTO quartos (categoria, status, capacidade, preco) VALUES (:category, :status, :capacity, :price)");
 
-        $sql->bindValue(':category', $category); 
         $sql->bindValue(':status', $status); 
         $sql->bindValue(':capacity', $capacity);  
-        $sql->bindValue(':price', $price); 
 
         $sql->execute();
 
